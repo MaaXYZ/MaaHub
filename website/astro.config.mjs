cd3262b49bb92efcd258a8f1dd3386ff3332eada
kqcoxn <mjy03730@163.com>
2026-06-12 21:27:22 +0800

    perf: SEO 优化

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL,
+  site: process.env.SITE_URL ?? 'https://maahub.github.io',
   base: process.env.SITE_BASE_URL ?? '/',
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), sitemap()]
 });
